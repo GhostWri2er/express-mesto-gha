f696980102180217a5d6202f5c547b7b84bdf6fd
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2022-07-17 18:12:55 +0500

    Тесты

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const getUsers = (req, res) => {
 };
 
 const getUserID = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
   .then(user => res.send({ data: user }))
   .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 }
