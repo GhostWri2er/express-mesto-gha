78182bb8fa3444ad812e11d5bf7787de50318c1e
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2022-07-17 18:16:48 +0500

    Тесты2

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { getUsers, getUserID, createUser, updateUser, updateAvatar } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/users/:userId', getUserID);
+router.get('/:userId', getUserID);
 router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
