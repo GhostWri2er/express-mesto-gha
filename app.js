e872e212260733cd2e0cc76912b1370e9ea10fa5
Ivan Mironovich <ivanmironovich@vk.com>
2023-02-16 14:31:51 +0500

    feat: Ошибка пути

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use((req, res, next) => {
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
-// app.use((req, res, next) => {
-//   next(res.status(404).send({ message: 'Страница не найдена' }));
-// });
+app.use((req, res, next) => {
+  next(res.status(404).send({ message: 'Страница не найдена' }));
+});
 
 
 app.listen(PORT, () => {
