c1fc9365c9db8ce34f56af6cfea96e1db2c3fef1
Ivan Mironovich <ivanmironovich@vk.com>
2023-02-16 17:19:30 +0500

    fix: fix all problem eslin

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-const { NOT_FOUND } = require('./errors/errors');
-const userRouter = require('./routes/users');
+const bodyParser = require('body-parser');
+// eslint-disable-next-line import/extensions
+const { NOT_FOUND } = require('./errors/errors.js');
 
+const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
@@ -13,12 +15,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use(express.json());
-app.use(express.json().urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '62d3dd9ad89632972e698684',
+    _id: '62d3dd9ad89632972e698684', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
